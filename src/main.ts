eaf6d600d432b0de9879c35b582bada709e2cb3b
mohamed-jilani <mohamedjilani551@gmail.com>
2025-01-26 17:10:34 +0100

    Activer CORS

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Activer CORS
+  app.enableCors({
+    origin: 'http://localhost:5173', // Origine autorisée (le frontend)
+    credentials: true,              // Si tu utilises des cookies/sessions
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // Méthodes autorisées
+    allowedHeaders: 'Content-Type, Accept, Authorization', // En-têtes autorisés
+  });
+
+  await app.listen(3000);
 }
 bootstrap();
